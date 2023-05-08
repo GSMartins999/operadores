f2ac85f88de05e192b8f2e7b3add9cccb784688a
Giovanni Souza <GSMartins999>
2023-05-08 16:59:42 -0300

    logicos

--- operadoreslogicos.js
+++ operadoreslogicos.js
@@ -6,17 +6,17 @@ const valor3 = true
 const valor4 = true
 const valor5 = false
 
-// // &&(só mostra valores verdadeiros sem ambos os lados forem true).
-// console.log("Os valores são:", valor1 && valor5) //false
-// console.log("Os valores são:", valor3 && valor4) //true
-// console.log("Os valores são:", valor2 && valor4) //false
-// console.log("Os valores são:", valor2 && valor5) //false
+// &&(só mostra valores verdadeiros sem ambos os lados forem true).
+console.log("Os valores são:", valor1 && valor5) //false
+console.log("Os valores são:", valor3 && valor4) //true
+console.log("Os valores são:", valor2 && valor4) //false
+console.log("Os valores são:", valor2 && valor5) //false
 
-// // || (Só mostra valores verdadeiros se ambos os lados forem false)
-// console.log("Os valores são:", valor1 || valor5) //true
-// console.log("Os valores são:", valor3 || valor4) //true
-// console.log("Os valores são:", valor2 || valor4) //true
-// console.log("Os valores são:", valor2 || valor5) //false
+// || (Só mostra valores verdadeiros se ambos os lados forem false)
+console.log("Os valores são:", valor1 || valor5) //true
+console.log("Os valores são:", valor3 || valor4) //true
+console.log("Os valores são:", valor2 || valor4) //true
+console.log("Os valores são:", valor2 || valor5) //false
 
 //! (Nega os valores)
 
